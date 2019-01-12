51700fd352676b74d88d88b04f476c0fa2388490
Mirzagayev Aydin <am180@hdm-stuttgart.de>
2019-01-13 00:00:02 +0100

    kleiner Zwischenstand, nur zur Absicherung

--- src/clj_snake/app.clj
+++ src/clj_snake/app.clj
@@ -1,14 +1,15 @@
 (ns clj-snake.app
   (:import (java.awt Color Dimension Font)
-           (javax.swing JPanel JFrame Timer JOptionPane JLabel JButton)
+           (javax.swing JPanel JFrame Timer JOptionPane JLabel JButton JTextField)
            (java.awt.event ActionListener KeyListener KeyEvent WindowListener))
   (:gen-class))
 
 
 ;; KONSTANTEN
+(def w_width "Fensterbreite" 550)
+(def w_height "Fensterhöhe" 550)
 
-(def w_width "Fensterbreite" 400)
-(def w_height "Fensterhöhe" 400)
+;; SWING OBJEKTE
 
 ;; COLORs
 (def background-color-splash (Color/decode "#DFDCE3"))
@@ -17,32 +18,38 @@
 (def background-color-game (Color/decode "#4ABDAC"))
 (def snake-color (Color/decode "#F78733"))
 (def apple-color (Color/decode "#FC4A1a"))
+(def text-color (Color/decode "#666666"))
 
 (defn say-hello []
   (JOptionPane/showMessageDialog
     nil "Hello, World!" "Greeting"
     JOptionPane/INFORMATION_MESSAGE))
 
-(def start-game (proxy [ActionListener] []
-           (actionPerformed [event] (say-hello))))
+(def btn-action (proxy [ActionListener] []
+                  (actionPerformed [event] (say-hello))))
+
 
 (defn spiel []
   "Hier beginnt die Spielkonfiguration"
-  (let [frame (JFrame. "Snake von am180")
-        panel (JPanel.)
-        label (JLabel. "Welcome in our snake game")
-        button (JButton. "START")]
-    (doto button
-      (.addActionListener start-game)
+  (let [frame (JFrame. "Snake game")
+        splash-panel (JPanel.)
+        welcome-label (JLabel. "Welcome in our snake game")
+        manual-label (JLabel. "follow this manual before you start the game")
+        start-button (JButton. "START")]
+    (doto start-button
+      (.addActionListener btn-action)
       (.setBackground background-color-game))
-    (doto panel
-      (.setOpaque true)
-      (.add label)
-      (.add button)
+    (doto splash-panel
+      (.setFocusable true)
+      (.add welcome-label)
+      (.add manual-label)
+      (.add start-button)
       (.setBackground background-color-splash))
     (doto frame
       (.setSize w_width w_height)
-      (.setContentPane panel)
+      (.setContentPane splash-panel)
+      (.setLocationRelativeTo nil)
+      (.setResizable false)
       (.setDefaultCloseOperation JFrame/EXIT_ON_CLOSE)
       (.addWindowListener
         (proxy [WindowListener] []
